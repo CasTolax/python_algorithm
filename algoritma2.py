cd808eb143124af9d498ce92c4a8b053ed3a6ce1
CasTolax <cantuncer32@gmail.com>
2025-08-09 10:26:22 +0300

    Update algoritma2.py

--- algoritma2.py
+++ algoritma2.py
@@ -1,10 +1,10 @@
 
 def en_uzun_ardisik_dizi(nums):
-    sayilar = set(nums)  # hızlı arama için
+    sayilar = set(nums) 
     en_uzun = 0
 
     for sayi in sayilar:
-        if (sayi - 1) not in sayilar:  # sadece ardışık serinin başlangıcında başla
+        if (sayi - 1) not in sayilar: 
             mevcut = sayi
             uzunluk = 1
 
@@ -19,4 +19,4 @@ def en_uzun_ardisik_dizi(nums):
 
 # Test
 num = [1, 2, 3, 400, 76, 45, 32, 56, 64, 78, 68, 899]
-print(en_uzun_ardisik_dizi(num))  # Çıktı: 3 → çünkü 1,2,3 ardışık
+print(en_uzun_ardisik_dizi(num))  
